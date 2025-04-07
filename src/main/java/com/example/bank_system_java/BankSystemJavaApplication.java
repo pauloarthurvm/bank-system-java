4f508dab4cd425e77729d868a27678234b73b8a8
Paulo Varassin <pauloarthurvm@hotmail.com>
2025-04-07 15:45:09 -0300

    first commit

---
+++ src/main/java/com/example/bank_system_java/BankSystemJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_system_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank_system_java/BankSystemJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_system_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
