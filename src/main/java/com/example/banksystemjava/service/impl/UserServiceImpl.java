19d5e82e727997978d6366e87eae01dbd4ab48bd
Paulo Varassin <pauloarthurvm@hotmail.com>
2025-04-08 11:25:31 -0300

    Implement controller

--- src/main/java/com/example/banksystemjava/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/banksystemjava/service/impl/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.example.banksystemjava.service.impl;
 import com.example.banksystemjava.model.User;
 import com.example.banksystemjava.repository.UserRepository;
 import com.example.banksystemjava.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.NoSuchElementException;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
@@ -21,7 +23,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User create(User user) {
-        if (userRepository.existsByAccounNumber(user.getAccount().getNumber())) {
+        if (userRepository.existsByAccountNumber(user.getAccount().getNumber())) {
             throw new IllegalArgumentException("This Account Number already exists");
         }
         return userRepository.save(user);
